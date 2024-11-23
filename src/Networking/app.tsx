b4a707b44bdbfcd4e538818d129b200af775f77c
Techpure <151980378+RS3QuestBuddy@users.noreply.github.com>
2024-11-22 20:24:34 -0600

    Added Features

---
+++ src/Networking/app.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route, HashRouter, Navigate, Outlet } from "react-router-dom";
+import { QuestPick } from "./../Pages/QuestPicker";
+function App() {
+  return (
+    <>
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/">
+            {/* Your default route */}
+            <Route index element={<QuestPick />} />
+          </Route>
+
+          <Route path="/QuestPage" element={""} />
+          <Route path="/*" element={<Navigate to="/" />} />
+          {/* Navigate to the default route if no URL matched */}
+        </Routes>
+      </HashRouter>
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/QuestPicker.tsx
@@ -0,0 +1,9 @@
+export const QuestPick: React.FC = () => {
+  return (
+    <>
+      <div>
+        <p>I am right here :D</p>
+      </div>
+    </>
+  );
+};
