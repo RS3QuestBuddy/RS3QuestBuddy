e6ae17027bbaa6865ee016147d9417928037bc86
RS3QuestBuddy <151980378+RS3QuestBuddy@users.noreply.github.com>
2024-11-25 18:57:07 -0600

    Delete Pages/Data Fetchers directory

--- Pages/Data Fetchers/Components/QuestAccordian.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-type AccordionProps = {
-    onClick?: (title: string) => void;
-};
-export declare const Accordion: React.FC<AccordionProps>;
-export {};
--- Pages/Data Fetchers/FetchQuestList.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export type questlist = {
-    quests: string[];
-};
-type QuestFetcherProps = {
-    onFetchComplete?: (fetched: boolean) => void;
-};
-export declare const QuestFetcher: React.FC<QuestFetcherProps>;
-export {};
